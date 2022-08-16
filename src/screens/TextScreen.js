7626af7044c90440cec999b3e46e1e732f7027e3
Jamar <jamar.arrington.git@gmail.com>
2022-08-15 20:42:19 -0500

    Updated state for text

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 
 const TextScreen = () => {
+    const [name, setName] = useState('');
+
     return <View>
+        <Text>Enter name:</Text>
         <TextInput 
           style={styles.input}
           autoCapitalize="none"
           autoCorrect={false}
+          value = {name}
+          onChangeText = {(newValue) => setName(newValue)  }
         />
+        <Text>My name is {name}</Text>
     </View>
 };
 
