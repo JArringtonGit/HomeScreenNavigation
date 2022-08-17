1bb2394bf6c944ec5116d16b10a39b4adc3e2b1d
Jamar <jamar.arrington.git@gmail.com>
2022-08-16 20:57:22 -0500

    checking if password is of certain length

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,18 +2,18 @@ import React, { useState } from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 
 const TextScreen = () => {
-    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
 
     return <View>
-        <Text>Enter name:</Text>
+        <Text>Enter Password:</Text>
         <TextInput 
           style={styles.input}
           autoCapitalize="none"
           autoCorrect={false}
-          value = {name}
-          onChangeText = {(newValue) => setName(newValue)  }
+          value = {password}
+          onChangeText = {(newValue) => setPassword(newValue)  }
         />
-        <Text>My name is {name}</Text>
+        {password.length < 4 ? <Text>Password cannot exceed 4 characters</Text> : null}
     </View>
 };
 
